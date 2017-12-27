7de866edb8c594524bf5b12abe9411ba698f72e0
Scott Helms <shelms@imageoptions.net>
2017-12-26 21:09:19 -0800

    BR-oo2 define initial ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,15 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    can :read, :all .
+    if user.present?
+      can :manage, Review, user_id: user.id
+      if user.admin?
+        can :manage, :all
+      end
+    end
+  end
+  
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
